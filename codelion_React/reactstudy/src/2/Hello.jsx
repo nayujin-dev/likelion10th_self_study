514a7098e8ec2f416a50beda808ab35093e85bba
Nayujin <00nyj@sookmyung.ac.kr>
2022-08-08 23:55:30 +0900

    feat: 2-3 inline style

---
+++ codelion_React/reactstudy/src/2/Hello.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hello(){
+    const PracticeStyle={
+        marginTop: '10px',
+        backgroundColor:'blue',
+    };
+    return (
+        <>
+            <div style={PracticeStyle}>HEllo</div>
+        </>
+    );
+}
+export default Hello;
